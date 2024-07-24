0afd47955e6197310365e0d00d5c9ef8728e9a72
Charon Wang <charonwangg@Gmail.com>
2024-07-23 17:10:13 -0700

    optimize retrieve_mentee and retrieve_keyword

--- retrieve_keyword.py
+++ retrieve_keyword.py
@@ -5,26 +5,62 @@ import requests
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 import yaml
-import pandas  as pd
+import pandas as pd
 from utils.find import HTMLFinder
 from retrieve_faculty import configure_logging
 from tqdm import tqdm
+
 
 def clean_scraped_text(text):
     text = re.sub(r'[^\S\r\n]+', ' ', text)
     text = re.sub(r'[^a-zA-Z0-9,.!?;:\s]', '', text)
     return text
 
+
 def clean_df(df):
     # TODO: clean the dataframe, remove the noise (mailto in email, etc.), fill the missing values (department)
     pass
 
-def retrieve_profile_text(path):
-    with open(path, 'r') as f:
-        soup = f.read()
-    soup = BeautifulSoup(soup, 'html.parser').text
+
+def retrieve_profile_text(html, online=False):
+    if online:
+        soup = BeautifulSoup(html.content, 'html.parser').text
+    else:
+        with open(html, 'r') as f:
+            soup = f.read()
+        soup = BeautifulSoup(soup, 'html.parser').text
     soup = clean_scraped_text(soup)
     return soup
+
+
+def process_profiles(df, profile_dir, html_finder, department, logger, entity_type):
+    if 'keyword' not in df.columns:
+        df['keyword'] = "{}"
+    df['department'] = df['department'].apply(lambda x: department if x == "{}" else x)
+
+    for i, row in tqdm(list(df.iterrows()), desc=f'Finding keywords for {entity_type} in {department}'):
+        if "[" in row['keyword'] and "]" in row['keyword']:
+            logger.info(f"Skipping {row['name']} as it already has keywords")
+            continue
+        official_path = os.path.join(profile_dir, row['name'], 'official_profile.html')
+        personal_path = os.path.join(profile_dir, row['name'], 'personal_profile.html')
+        official_text = retrieve_profile_text(official_path)
+        try:
+            personal_text = retrieve_profile_text(personal_path)
+        except Exception as e:
+            logger.error(f"{row['name']} does not have a personal profile")
+            personal_text = ""
+        # merge the two texts
+        prompt = open('prompts/find_research_keywords_in_html.txt', 'r').read()
+        html_content = ("\nHere is the personal website of this researcher in the department/university website:\n"
+                        "[official_html]\nHere is the lab/personal website of this researcher:\n[personal_html]\n")
+        html_content = html_content.replace('[official_html]', official_text)
+        html_content = html_content.replace('[personal_html]', personal_text)
+        keyword = html_finder.find_keywords_in_html(html_content, previous_info=row.to_dict())
+        df.at[i, 'keyword'] = keyword
+
+    return df
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -48,33 +84,14 @@ if __name__ == '__main__':
         try:
             data_dir = cfg[department]['data_dir']
             profile_dir = os.path.join(data_dir, 'profiles')
-            df = pd.read_csv(os.path.join(data_dir, 'faculty_profiles.csv'))
-            if 'keyword' not in df.columns:
-                df['keyword'] = "{}"
-            df['department'] = df['department'].apply(lambda x: department if x == "{}" else x)
-            for i, row in tqdm(list(df.iterrows()), desc=f'Finding keywords for {department}'):
-                if row['keyword'] != "{}":
-                    logger.info(f"Skipping {row['name']} as it already has keywords")
-                    continue
-                official_path = os.path.join(profile_dir, row['name'], 'official_profile.html')
-                personal_path = os.path.join(profile_dir, row['name'], 'personal_profile.html')
-                official_text = retrieve_profile_text(official_path)
-                try:
-                    personal_text = retrieve_profile_text(personal_path)
-                except Exception as e:
-                    print(f"{row['name']} does not have a personal profile")
-                    personal_text = ""
-                # merge the two texts
-                prompt = open('prompts/find_research_keywords_in_html.txt', 'r').read()
-                html_content = ("\nHere is the personal website of this researcher in the department/university website:\n"
-                                "[official_html]\nHere is the lab/personal website of this researcher:\n[personal_html]\n")
-                html_content = html_content.replace('[official_html]', official_text)
-                html_content = html_content.replace('[personal_html]', personal_text)
-                keyword = html_finder.find_keywords_in_html(html_content, previous_info=row.to_dict())
-                df.at[i, 'keyword'] = keyword
-            df.to_csv(os.path.join(data_dir, 'faculty_profiles.csv'), index=False)
+            faculty_df = pd.read_csv(os.path.join(data_dir, 'faculty_profiles.csv'))
+            mentee_df = pd.read_csv(os.path.join(data_dir, 'mentee_profiles.csv'))
+
+            faculty_df = process_profiles(faculty_df, profile_dir, html_finder, department, logger, 'faculty')
+            mentee_df = process_profiles(mentee_df, profile_dir, html_finder, department, logger, 'mentee')
+
+            faculty_df.to_csv(os.path.join(data_dir, 'faculty_profiles.csv'), index=False)
+            mentee_df.to_csv(os.path.join(data_dir, 'mentee_profiles.csv'), index=False)
         except Exception as e:
             logger.error(f"{e}, {department} might not be collected")
             continue
-
-
--- retrieve_mentee.py
+++ retrieve_mentee.py
@@ -11,34 +11,13 @@ import os
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from datetime import datetime
 import ast
-from .retrieve_faculty import name_in_column
-
-
-# Configure logging
-def configure_logging(department):
-    log_dir = 'logs'
-    os.makedirs(log_dir, exist_ok=True)
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    log_file = os.path.join(log_dir, f'{current_time}_{department}_phds_retrieval.log')
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    if not logger.handlers:
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
-
-    return logger
+from retrieve_faculty import name_in_column, configure_logging
 
 
 def df_to_mentee_list(df):
     df_dict = df.to_dict(orient='records')
     new_df_dict = []
     for entry in df_dict:
-        print(entry)
         new_df_dict.append({k: ast.literal_eval(v) if '{' in v and '}' in v else v for k, v in entry.items()})
 
     mentees = []
@@ -60,13 +39,17 @@ def df_to_mentee_list(df):
 def fetch_mentee_info(entry, api_key, crawler_cfg, df, logger):
     web_browser = WebBrowser(headless=True, sleep_time=crawler_cfg['sleep_time'])
     html_finder = HTMLFinder(api_key=api_key, model=crawler_cfg['model'],
-                             token_limit_per_minute=crawler_cfg['token_limit_per_minute'])
+                             token_limit_per_minute=crawler_cfg['token_limit_per_minute'], logger=logger)
 
     logger.info(f"Fetching mentee information for {entry['name']}")
 
-    search_query = f"{entry['name']} {entry['university']}"
+    search_query = f"{entry['university']} {entry['name']}"
+
+    google_prompt = (f"Here is the previous gathered information: {entry}, "
+                     f"include the previous 'website' in the return if you find it align with "
+                     f"your search result analysis.")
     personal_soup = html_finder.find_relevant_content_from_google(web_browser, search_query,
-                                                                  previous_info=entry["website"])
+                                                                  previous_info=google_prompt)
     entry = html_finder.find_mentee_info_in_html(personal_soup, previous_info=entry)
 
     logger.info(f"Info after gathering from personal profile: {entry}")
@@ -74,9 +57,8 @@ def fetch_mentee_info(entry, api_key, crawler_cfg, df, logger):
     # Save the official and personal profile to files
     profile_path = os.path.join(profile_dir, entry["name"])
     os.makedirs(profile_path, exist_ok=True)
-    if entry.get('website'):
-        with open(os.path.join(profile_path, 'personal_profile.html'), 'w', encoding='utf-8') as file:
-            file.write(str(personal_soup))
+    with open(os.path.join(profile_path, 'personal_profile.html'), 'w', encoding='utf-8') as file:
+        file.write(str(personal_soup))
 
     web_browser.quit()
 
@@ -120,26 +102,32 @@ if __name__ == '__main__':
     if os.environ.get('API_KEY') is None:
         raise ValueError("API_KEY is not set")
 
-    cfg = yaml.safe_load(open(args.config, 'r'))[args.department]
+    cfg = yaml.safe_load(open(args.config, 'r'))
     crawler_cfg = yaml.safe_load(open('configs/crawler.yaml', 'r'))
     api_key = os.environ.get('API_KEY')
 
-    # Configure logging
-    logger = configure_logging(args.department)
+    # Initialization
+    if args.department != 'all':
+        cfg = {args.department: cfg[args.department]}
+    logger = configure_logging(args.department, type='mentee')
 
-    data_dir = cfg['data_dir']
-    profile_dir = os.path.join(data_dir, 'profiles')
-    os.makedirs(data_dir, exist_ok=True)
-    os.makedirs(profile_dir, exist_ok=True)
+    for department in cfg.keys():
+        try:
+            data_dir = cfg[department]['data_dir']
+            profile_dir = os.path.join(data_dir, 'profiles')
+            os.makedirs(data_dir, exist_ok=True)
+            os.makedirs(profile_dir, exist_ok=True)
 
-    logger.info(
-        f"Fetching Graduate students/Postdocs' contacts from the gathered faculties in the {args.department} department.")
-    if os.path.exists(os.path.join(data_dir, 'faculty_profiles.csv')):
-        df = pd.read_csv(os.path.join(data_dir, 'faculty_profiles.csv'))
-        mentee_entries = df_to_mentee_list(df)
-    else:
-        logger.error('No faculty profiles found. Please run retrieve_faculty.py first.')
-        exit(1)
+            logger.info(
+                f"Fetching Graduate students/Postdocs' contacts from the gathered faculties in the {args.department} department.")
+            if os.path.exists(os.path.join(data_dir, 'faculty_profiles.csv')):
+                df = pd.read_csv(os.path.join(data_dir, 'faculty_profiles.csv'))
+                mentee_entries = df_to_mentee_list(df)
+            else:
+                logger.error('No faculty profiles found. Please run retrieve_faculty.py first.')
+                exit(1)
 
-    mentee_df = build_mentee_df(mentee_entries, api_key, crawler_cfg, logger)
-    logger.info(f"Finished processing all mentee profiles for the {args.department} department.")
+            mentee_df = build_mentee_df(mentee_entries, api_key, crawler_cfg, logger)
+            logger.info(f"Finished processing all mentee profiles for the {args.department} department.")
+        except Exception as e:
+            logger.error(f"An error occurred while fetching mentee profiles: {e}")
